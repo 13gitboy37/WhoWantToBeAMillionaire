f8c68c1e878905749f7141331bc97c4f121e9aa3
nikitamoshentsev <trane0909@bk.ru>
2022-05-13 15:05:33 +0300

    Initial Commit

---
+++ WhoWantToBeAMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantToBeAMillionaire
+//
+//  Created by Никита Мошенцев on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
